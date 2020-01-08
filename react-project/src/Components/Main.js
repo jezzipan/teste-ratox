b7095100ee9bac20f652a8203cd861e699dda25b
Jessica Oliveira <jessica.leute@gmail.com>
2020-01-08 17:58:53 -0300

    Fixed: Tarefa components

--- react-project/src/Components/Main.js
+++ react-project/src/Components/Main.js
@@ -1,40 +1,50 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 // Form
-import {FaPlus} from 'react-icons/fa';
-import './Main.css';
+import { FaPlus } from "react-icons/fa";
 
+// Tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
+import "./Main.css";
 
 export default class Main extends Component {
-    state = {
-      novaTarefa: "",
-    };
+  state = {
+    novaTarefa: "",
+    tarefas: ["Fazer café", "Beber água", "Estudar"]
+  };
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({
-      novaTarefa: e.target.value,
-    })
-
-  }
-
+      novaTarefa: e.target.value
+    });
+  };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
-    return(
+    return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-         <form action="#" className="form">
-           <input
-           onChange={this.handleChange}
-           type="text"
-           value={ novaTarefa } />
-           <button type="submit">
-           <FaPlus />
-           </button>
-         </form>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+            <div>
+              <FaEdit className="edit" />
+              <FaWindowClose className="delete" />
+            </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
