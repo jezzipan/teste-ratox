727d43d44f79d5fe5eace87889ac70aefd6882ed
Jessica Oliveira <jessica.leute@gmail.com>
2020-01-08 17:31:53 -0300

    Fixed: Formulario estilizado

--- react-project/src/Components/Main.js
+++ react-project/src/Components/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+
+// Form
+import {FaPlus} from 'react-icons/fa';
 import './Main.css';
 
 
@@ -23,9 +26,14 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-         <form action="#">
-           <input onChange={this.handleChange} type="text" />
-           <button type="submit"> Enviar</button>
+         <form action="#" className="form">
+           <input
+           onChange={this.handleChange}
+           type="text"
+           value={ novaTarefa } />
+           <button type="submit">
+           <FaPlus />
+           </button>
          </form>
       </div>
     );
